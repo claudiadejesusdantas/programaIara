989b8b9fc5c6f18822a019260f5fc69d6e528f5c
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-12-10 16:01:37 -0300

    Inclusão do footer.

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div>
+            <footer>&copy;
+                Desenvolvido por Claudia de Jesus Dantas
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
