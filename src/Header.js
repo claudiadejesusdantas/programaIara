fbe05791f0da9dc30b361804b13f8fe0b8898cce
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-12-10 18:33:41 -0300

    Melhoria e inclusão do ícones no cabeçalho.

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import './Header.css'
+import HeaderOption from './HeaderOption'
+import HomeIcon from '@mui/icons-material/Home';
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import PersonIcon from '@mui/icons-material/Person';
 
 function Header() {
-  return (
-    <>
-        <div className='header'>
-            <h1>This is a Header</h1>
-        </div>
-    </>
-  )
+    return (
+            <div className='header'>
+                <div className='header__left'>
+                    <img src='../assets/catmermaid_color2.png' alt='Gata com rabo de sereia.' />
+                </div>
+                <div className='header__left'>
+                    <h1>ProgramaIara</h1>
+                </div>
+
+                <div className='header__right'>
+                    <HeaderOption Icon={HomeIcon} title={"Home"} />
+                    <HeaderOption Icon={LocalLibraryIcon} title={"Cursos"} />
+                    <HeaderOption Icon={WorkOutlineIcon} title={"Vagas"} />
+                    <HeaderOption Icon={PersonIcon} title={"Network"} />
+
+                </div>
+
+
+            </div>
+    )
 }
 
 export default Header
