4a780512d0f060c764e5ac5b7fe466a38ad275fc
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-12-12 10:36:21 -0300

    Inclusão de ícones da postagem do feed.

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import './Feed.css'
 import CreateIcon from '@mui/icons-material/Create';
+import InputOption from './InputOption';
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import PersonIcon from '@mui/icons-material/Person';
 
 function Feed() {
   return (
@@ -15,7 +19,10 @@ function Feed() {
             </div>
 
             <div className='feed__inputOptions'>
-                
+                <InputOption Icon={LocalLibraryIcon} title='Cursos' color="#70B5"/>
+                <InputOption Icon={WorkOutlineIcon} title='Cursos' color="#e7a"/>
+                <InputOption Icon={PersonIcon} title='Cursos' color="#7fc"/>
+
             </div>
         </div>
     </div>
