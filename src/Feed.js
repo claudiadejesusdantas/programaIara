b268eecc03b8c990ee156918c252b8f00f59ba34
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-12-12 10:05:21 -0300

    Inclusão do feed.

---
+++ src/Feed.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Feed.css'
+import CreateIcon from '@mui/icons-material/Create';
+
+function Feed() {
+  return (
+    <div className='feed'>
+        <div className='feed_inputContainer'>
+            <div className='feed__input'>
+                <CreateIcon/>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Feed
