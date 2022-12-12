0d5547c6e3462117272cb28f85d0cd5830598997
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-12-12 10:21:54 -0300

    Inclusão da barra de postagem.

--- src/Feed.js
+++ src/Feed.js
@@ -8,7 +8,14 @@ function Feed() {
         <div className='feed_inputContainer'>
             <div className='feed__input'>
                 <CreateIcon/>
+                <form>
+                    <input type="text"/>
+                    <button type="submit"> Enviar </button>
+                </form>
+            </div>
 
+            <div className='feed__inputOptions'>
+                
             </div>
         </div>
     </div>
