ba3c06a8a43254a118d09919b42d08f18029fbb0
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-12-13 17:25:14 -0300

    Inclusão das normas da Programaria.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -26,8 +26,20 @@ function Sidebar() {
                 <p>"Eu recomendo a todos que tentem programar pelo menos uma vez ...especialmente as meninas." - Malala Yoursafzai </p>
             </div>
             <div className='sidebar__bottom'>
-                <h2> Frase </h2>
-                <p>  </p>
+                <ul> Diretrizes da plataforma: 
+                    <li>🧜‍♀️ Todas devem ser respeitadas, independentemente de religião, condição física, entre outros. Então não será tolerado qualquer caso de xenofobia, racismo, homofobia, sexismo ou qualquer tipo de discurso de ódio! Passível de ban imediato!</li>
+                    <li>🧜‍♀️ Antes de divulgar algo, consulte por gentileza se a fonte é confiável.</li>
+                    <li>🧜‍♀️ Links, imagens, mensagens, vídeos que contenham material sensível (gore, pornográfico, mensagem de ódio, etc.) não serão permitidos aqui!</li>
+                    <li>🧜‍♀️ Sem imagens e/ou mensagens que contenham informação pessoais de outras pessoas! </li>
+                    <li>🧜‍♀️ Mensagens com o intuito de causar irritação, serem negativas, que visam somente criar transtornos não serão toleradas!</li>
+                    <li>🧜‍♀️ Qualquer tentativa de burlar uma punição levará a ban imediato!</li>
+                    <li>🧜‍♀️ Tenham bom senso, ética e respeito!</li>
+                    <li>🧜‍♀️ Esperamos que esse seja um ambiente de sororidade, a qual cada uma pode ajudar a outra! 💜</li>
+                    
+                    
+
+                </ul>
+                
 
             </div>
         </div>
