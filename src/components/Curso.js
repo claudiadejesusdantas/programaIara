864d7574a66a10c03d791c142ef84d7846688658
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-12-13 22:40:56 -0300

    Inclusão de componentes na página cursos e banco de dados cursos do firebase.

---
+++ src/components/Curso.js
@@ -0,0 +1,36 @@
+import { Avatar } from '@mui/material'
+import React, {forwardRef} from 'react'
+import InputOption from '../InputOption'
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import InsertCommentIcon from '@mui/icons-material/InsertComment';
+import ShareIcon from '@mui/icons-material/Share';
+import ReactLinkify from 'react-linkify';
+import './Curso.css'
+
+const Curso = forwardRef(({name, description, message, photoUrl}, ref) => {
+    return (
+        <div ref={ref} className='curso'>
+            <div className='curso__header'>
+                <Avatar src={photoUrl}>{!name ? "?" : name[0]}</Avatar>
+                <div className='curso__info'>
+                    <h2>{name}</h2>
+                    <p>{description}</p>
+                </div>
+            </div>
+
+            <div className='curso__body'>
+                <ReactLinkify componentDecorator={(decoratedHref, decoratedText, key) => ( <a target="blank" href={decoratedHref} key={key}> {decoratedText} </a> )}> 
+                    <p>{message}</p>
+                </ReactLinkify>
+            </div>
+
+            <div className='curso__buttons'>
+                <InputOption Icon={ThumbUpIcon} title="Gostei" color="var(--cinza)"/>
+                <InputOption Icon={InsertCommentIcon} title="Comentar" color="var(--cinza)"/>
+                <InputOption Icon={ShareIcon} title="Compartilhar" color="var(--cinza)"/>
+            </div>
+        </div>
+    )
+})
+
+export default Curso
--- src/pages/Cursos.js
+++ src/pages/Cursos.js
@@ -1,13 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../Sidebar'
+import './Cursos.css'
+import CreateIcon from '@mui/icons-material/Create';
+import InputOption from '../InputOption';
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import PersonIcon from '@mui/icons-material/Person';
+// import Post from '../Post';
+import Curso from '../components/Curso';
+import { db } from '../firebase';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
+import FlipMove from 'react-flip-move';
 
 function Cursos() {
-  return (
-    <div className='app__body'>
-            <Sidebar/>
-            <h2>I am Cursos!!!</h2>
-    </div>
-  )
+    const user = useSelector(selectUser);
+    const [input, setInput] = useState('');
+    const [cursos, setCursos] = useState([]);
+
+    useEffect(() => {
+        db.collection("cursos").orderBy('timestamp', 'desc').onSnapshot((snapshot) =>
+            setCursos(snapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data(),
+            }))
+            )
+        );
+    }, []);
+
+    const sendPost = (e) => {
+        e.preventDefault();
+
+        db.collection("cursos").add({
+            name: user.displayName,
+            description: user.email,
+            message: input,
+            photoURL: "",
+            timestamp: new Date().getTime()
+        });
+        setInput("");
+    };
+
+    return (
+        <div className='cursos'>
+            <div className='cursos_inputContainer'>
+                <div className='cursos__input'>
+                    <CreateIcon />
+                    <form>
+                        <input value={input} onChange={e => setInput(e.target.value)} type="text" />
+                        <button onClick={sendPost} type="submit"> Enviar </button>
+                    </form>
+                </div>
+
+                <div className='cursos__inputOptions'>
+                    <InputOption Icon={LocalLibraryIcon} title='Cursos' color="var(--cursos)" />
+                </div>
+            </div>
+
+            <FlipMove>
+            {cursos.map(({ id, data: { name, description, message, photoUrl } }) =>
+                <Curso
+                    key={id}
+                    name={name}
+                    description={description}
+                    message={message}
+                    photoUrl={photoUrl}
+                />
+            )}
+            </FlipMove>
+        </div>
+    )
 }
 
 export default Cursos
