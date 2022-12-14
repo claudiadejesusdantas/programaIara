4d5ac938c19873b1e1d9e284de5811871b41cf8b
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-12-13 23:29:23 -0300

    Retirada de botões no feed.

--- src/Feed.js
+++ src/Feed.js
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from 'react'
 import './Feed.css'
 import CreateIcon from '@mui/icons-material/Create';
 import InputOption from './InputOption';
-import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
-import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
-import PersonIcon from '@mui/icons-material/Person';
 import Post from './Post';
 import { db } from './firebase';
 import { useSelector } from 'react-redux';
@@ -48,12 +45,6 @@ function Feed() {
                         <input value={input} onChange={e => setInput(e.target.value)} type="text" />
                         <button onClick={sendPost} type="submit"> Enviar </button>
                     </form>
-                </div>
-
-                <div className='feed__inputOptions'>
-                    <InputOption Icon={LocalLibraryIcon} title='Cursos' color="var(--cursos)" />
-                    <InputOption Icon={WorkOutlineIcon} title='Vagas' color="var(--vagas)" />
-                    <InputOption Icon={PersonIcon} title='Network' color="var(--network)" />
                 </div>
             </div>
 
