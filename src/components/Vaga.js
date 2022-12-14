2f33b9b96ab1d8cc80d39c53ecfe1ec8b794e19f
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-12-13 23:07:43 -0300

    Inclusão de componentes na página vagas e banco de dados vagas do firebase.

---
+++ src/components/Vaga.js
@@ -0,0 +1,36 @@
+import { Avatar } from '@mui/material'
+import React, {forwardRef} from 'react'
+import InputOption from '../InputOption'
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import InsertCommentIcon from '@mui/icons-material/InsertComment';
+import ShareIcon from '@mui/icons-material/Share';
+import ReactLinkify from 'react-linkify';
+import './Vaga.css'
+
+const Vaga = forwardRef(({name, description, message, photoUrl}, ref) => {
+    return (
+        <div ref={ref} className='vaga'>
+            <div className='vaga__header'>
+                <Avatar src={photoUrl}>{!name ? "?" : name[0]}</Avatar>
+                <div className='vaga__info'>
+                    <h2>{name}</h2>
+                    <p>{description}</p>
+                </div>
+            </div>
+
+            <div className='vaga__body'>
+                <ReactLinkify componentDecorator={(decoratedHref, decoratedText, key) => ( <a target="blank" href={decoratedHref} key={key}> {decoratedText} </a> )}> 
+                    <p>{message}</p>
+                </ReactLinkify>
+            </div>
+
+            <div className='vaga__buttons'>
+                <InputOption Icon={ThumbUpIcon} title="Gostei" color="var(--cinza)"/>
+                <InputOption Icon={InsertCommentIcon} title="Comentar" color="var(--cinza)"/>
+                <InputOption Icon={ShareIcon} title="Compartilhar" color="var(--cinza)"/>
+            </div>
+        </div>
+    )
+})
+
+export default Vaga
--- src/pages/Vagas.js
+++ src/pages/Vagas.js
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../Sidebar'
+import CreateIcon from '@mui/icons-material/Create';
+import InputOption from '../InputOption';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import { db } from '../firebase';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
+import FlipMove from 'react-flip-move';
+import Vaga from '../components/Vaga';
+import './Vagas.css'
 
 function Vagas() {
+  const user = useSelector(selectUser);
+  const [input, setInput] = useState('');
+  const [vagas, setVagas] = useState([]);
+
+  useEffect(() => {
+    db.collection("vagas").orderBy('timestamp', 'desc').onSnapshot((snapshot) =>
+      setVagas(snapshot.docs.map(doc => ({
+        id: doc.id,
+        data: doc.data(),
+      }))
+      )
+    );
+  }, []);
+
+  const sendPost = (e) => {
+    e.preventDefault();
+
+    db.collection("vagas").add({
+      name: user.displayName,
+      description: user.email,
+      message: input,
+      photoURL: "",
+      timestamp: new Date().getTime()
+    });
+    setInput("");
+  };
+
   return (
-    <div className='app__body'>
-            <Sidebar/>
-            <h2>I am Vagas!!!</h2>
+    <div className='vagas'>
+      <div className='vagas_inputContainer'>
+        <div className='vagas__input'>
+          <CreateIcon />
+          <form>
+            <input value={input} onChange={e => setInput(e.target.value)} type="text" />
+            <button onClick={sendPost} type="submit"> Enviar </button>
+          </form>
+        </div>
+
+        <div className='vagas__inputOptions'>
+          <InputOption Icon={WorkOutlineIcon} title='Vagas' color="var(--vagas)" />
+        </div>
+      </div>
+
+      <FlipMove>
+        {vagas.map(({ id, data: { name, description, message, photoUrl } }) =>
+          <Vaga
+            key={id}
+            name={name}
+            description={description}
+            message={message}
+            photoUrl={photoUrl}
+          />
+        )}
+      </FlipMove>
     </div>
   )
 }
