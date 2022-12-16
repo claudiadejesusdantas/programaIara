6959b40d4f57624a19c201d27b7085e635bb0e67
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-12-16 16:39:55 -0300

    Mudança de estilo na página network.

--- src/pages/Network.js
+++ src/pages/Network.js
@@ -39,32 +39,39 @@ function Network() {
   };
 
   return (
-    <div className='network'>
-      <div className='network_inputContainer'>
-        <div className='network__input'>
-          <CreateIcon />
-          <form>
-            <input placeholder='Aqui você se apresenta, coloca os links das suas opções de contato e adiciona outras amigas.' value={input} onChange={e => setInput(e.target.value)} type="text" />
-            <button onClick={sendPost} type="submit"> Enviar </button>
-          </form>
+    <div className='app__body'>
+      <div className='network'>
+
+        <div className='network_inputContainer'>
+        <div className='container__cabecalhoNetwork'>
+        <img className='imgNetwork' src='../assets/undraw_happy_women_day_fbjt network.svg' alt='#' />
+
+          <div className='network__input'>
+            <CreateIcon />
+            <form>
+              <input placeholder='Aqui você se apresenta, coloca os links das suas opções de contato e adiciona outras amigas.' value={input} onChange={e => setInput(e.target.value)} type="text" />
+              <button onClick={sendPost} type="submit"> Enviar </button>
+            </form>
+          </div>
+          </div>
+
+          <div className='network__inputOptions'>
+            <InputOption Icon={PersonIcon} title='Network' color="var(--network)" />
+          </div>
         </div>
 
-        <div className='network__inputOptions'>
-          <InputOption Icon={PersonIcon} title='Network' color="var(--network)" />
-        </div>
+        <FlipMove>
+          {network.map(({ id, data: { name, description, message, photoUrl } }) =>
+            <People
+              key={id}
+              name={name}
+              description={description}
+              message={message}
+              photoUrl={photoUrl}
+            />
+          )}
+        </FlipMove>
       </div>
-
-      <FlipMove>
-        {network.map(({ id, data: { name, description, message, photoUrl } }) =>
-          <People
-            key={id}
-            name={name}
-            description={description}
-            message={message}
-            photoUrl={photoUrl}
-          />
-        )}
-      </FlipMove>
     </div>
   )
 }
