a40c0e9b2e01ae256b274c358a30bdb76342c7ce
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-12-16 16:40:22 -0300

    Mudança de estilo na página vagas.

--- src/pages/Vagas.js
+++ src/pages/Vagas.js
@@ -39,33 +39,41 @@ function Vagas() {
   };
 
   return (
-    <div className='vagas'>
-      <div className='vagas_inputContainer'>
-        <div className='vagas__input'>
-          <CreateIcon />
-          <form>
-            <input value={input} onChange={e => setInput(e.target.value)} type="text" placeholder='Aqui você coloca as vagas de emprego, oportunidades freelance, clt, pj... Coloque o máximo de informações como nome da empresa, requisitos etc.' />
-            <button onClick={sendPost} type="submit"> Enviar </button>
-          </form>
+    <div className='app__body'>
+      <div className='vagas'>
+
+        <div className='vagas_inputContainer'>
+          <div className='container__cabecalhoVagas'>
+            <img className='imgVagas' src='../assets/undraw_learning_sketching_nd4f.svg' alt='#' />
+
+            <div className='vagas__input'>
+              <CreateIcon />
+              <form>
+                <input value={input} onChange={e => setInput(e.target.value)} type="text" placeholder='Aqui você coloca as vagas de emprego, oportunidades freelance, clt, pj... Coloque o máximo de informações como nome da empresa, requisitos etc.' />
+                <button onClick={sendPost} type="submit"> Enviar </button>
+              </form>
+            </div>
+          </div>
+
+          <div className='vagas__inputOptions'>
+            <InputOption Icon={WorkOutlineIcon} title='Vagas' color="var(--vagas)" />
+          </div>
         </div>
 
-        <div className='vagas__inputOptions'>
-          <InputOption Icon={WorkOutlineIcon} title='Vagas' color="var(--vagas)" />
-        </div>
+        <FlipMove>
+          {vagas.map(({ id, data: { name, description, message, photoUrl } }) =>
+            <Vaga
+              key={id}
+              name={name}
+              description={description}
+              message={message}
+              photoUrl={photoUrl}
+            />
+          )}
+        </FlipMove>
       </div>
-
-      <FlipMove>
-        {vagas.map(({ id, data: { name, description, message, photoUrl } }) =>
-          <Vaga
-            key={id}
-            name={name}
-            description={description}
-            message={message}
-            photoUrl={photoUrl}
-          />
-        )}
-      </FlipMove>
     </div>
+
   )
 }
 
